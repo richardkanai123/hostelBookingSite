63d4c4463900a2ccf340e0984780b55ebb8363b3
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-05-10 10:31:39 +0300

    env fix 2

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     appDir: true,
   },
 
+  // vercel env variables
+  env: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  }
+
 }
 
 module.exports = nextConfig
